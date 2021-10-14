fd5dd2a3d65286b008ca51b71c199c2fb9766aeb
Mauricio <mauriio.salazar721@gmail.com>
2021-10-13 23:28:11 -0500

    Update usuarios.js
    Update Usuarios Preview

--- src/components/pages/usuarios/usuarios.js
+++ src/components/pages/usuarios/usuarios.js
@@ -3,43 +3,91 @@ import "./usuarios.css"
 import Footer from '../../navegacion/Footer/Footer';
 import Navbar1 from '../../navegacion/Navbar/Navbar1';
 import { Link } from 'react-router-dom'
+import $ from "jquery"
 
 
 const Users = () => {
+    $(Document).ready(function() {
+
+        $("tr:first-child").append('<th class="actionsCol" contenteditable="false">Actions</th>');
+        $("tr:not(:first-child)").append('<td class="finalActionsCol"><i class="fa fa-plus-circle" aria-hidden="true"></i> <i class="fa fa-minus-circle" aria-hidden="true"></i> <i class="fa fa-pencil-square-o" aria-hidden="true"></i> </td>');
+      
+        $("table").on("click", ".fa-plus-circle", function() {
+          $(this).closest('tr').after('<tr><td class="idRow" contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td class="finalActionsCol"><i class="fa fa-plus-circle" aria-hidden="true"></i> <i class="fa fa-minus-circle" aria-hidden="true"></i> <i class="fa fa-pencil-square-o" aria-hidden="true"></i> </td></tr>');
+        });
+      
+        $("#addRow").on("click", function() {
+          $("table").append('<tr><td class="idRow" contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td contenteditable="false">-</td><td class="finalActionsCol"><i class="fa fa-plus-circle" aria-hidden="true"></i> <i class="fa fa-minus-circle" aria-hidden="true"></i> <i class="fa fa-pencil-square-o" aria-hidden="true"></i> </td></tr>');
+        });
+      
+        $("table").on("click", ".fa-minus-circle", function() {
+          if (prompt("Estas seguro de Borrar el Usuario? escribe 'y' para confirmar") == "y") {
+            $(this).closest('tr').remove();
+          } else {}
+        });
+      
+        $("table").on("click", ".fa-pencil-square-o, .fa-floppy-o", function() {
+          var thisRow = $(this).parent().siblings();
+          var editOn = $(this).hasClass("editMode");
+      
+          $('td:last-child').attr('contenteditable', 'false');
+          $('td:last-child').css('background-color', 'transparent');
+      
+          if (editOn == false) {
+            $(thisRow).attr('contenteditable', 'true');
+            $(thisRow).css('background-color', '#ffc9c9');
+            $(this).removeClass("fa-pencil-square-o");
+            $(this).addClass("fa-floppy-o editMode");
+          } else if (editOn == true) {
+            $(thisRow).attr('contenteditable', 'false');
+            $(thisRow).css('background-color', 'transparent');
+            $(this).removeClass("fa-floppy-o editMode");
+            $(this).addClass("fa-pencil-square-o");
+          }
+        });
+      
+        $('th', this).dblclick(function() {
+          $(this).attr("contenteditable", "true");
+        });
+        $('th', this).mouseout(function() {
+          $(this).attr("contenteditable", "false");
+        });
+      
+      });
+    
     return (
         <div>
             <div>
             <Navbar1 value={{ background: '#D0A2FE' }} 
             attribute={{
-                nombre1: 'VENTAS',
-                nombre2: 'PRODUCTOS',
-                nombre3: 'USUARIOS',
+                nombre1: 'USUARIOS',
+                nombre2: 'VENTAS',
+                nombre3: 'PRODUCTOS',               
                 nombre4: 'Cerrar Sesion',
-                to1: '/ventasAdmin',
-                to2: '/productoAdmin',
-                to3: '/usuariosAdmin',
+                to1: '/usuariosAdmin',
+                to2: '/ventasAdmin',
+                to3: '/productoAdmin',
                 to4: '/',
             }}/>  
-            </div>
-              
+            </div>  
             <div className="sample one horizontal caja1">
 
-        <input type="text" name="search" placeholder="search"/>
+                <input type="text" name="search" placeholder="search"/>
         
-      </div>
-      <div className="dropdown horizontal caja2">
+            </div>
+         <div className="dropdown horizontal caja2">
           <button className="algo btn btn-success btn-search btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Consulta
           </button>
           <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <li><Link className="dropdown-item" to="#">Identificador de la venta</Link></li>
-            <li><Link className="dropdown-item" to="#">ID del cliente</Link></li>
-            <li><Link className="dropdown-item" to="#">Nombre del cliente</Link></li>
+            <li><Link className="dropdown-item" to="#">ID Usuario</Link></li>
+            <li><Link className="dropdown-item" to="#">Nombre</Link></li>
+            <li><Link className="dropdown-item" to="#">Rol</Link></li>
           </div>
         </div>
             
 
-            <div class="container">
+            <div class="containerUser">
                 <table class="tableContents">
                 <tr>
                     <th id="IDCol">User ID</th>
