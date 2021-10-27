ede967837e470c30abbf77d12d13e91855a05820
hjosemejia <63750154+hjosemejia@users.noreply.github.com>
2021-10-27 01:25:08 -0500

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -5,8 +5,8 @@ const Schema  = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     //name: { type: String, required: true },
-    user_id: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
+    //user_id: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: {
